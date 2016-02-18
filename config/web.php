4012c6ec3400c1ddd85fdcf06d2d449880d40163
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-18 10:05:10 +0100

    Default theme

--- config/web.php
+++ config/web.php
@@ -3,47 +3,56 @@
 $params = require(__DIR__ . '/params.php');
 
 $config = [
-    'id' => 'pype',
-    'basePath' => dirname(__DIR__),
-    'defaultRoute' => '/mdpages/default/index',
-    'layout' => '@jacmoe/mdpages/views/layouts/main.php',
-    'bootstrap' => ['log'],
-    'components' => [
-            'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'PaldaMQ2zFEsAhv2SPRlp2it_XNnSuJz',
-        ],
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-        'user' => [
-            'identityClass' => '\yii\web\IdentityInterface',
-        ],
-        'errorHandler' => [
-            'errorAction' => '/mdpages/default/error',
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
+  'id' => 'pype',
+  'basePath' => dirname(__DIR__),
+  'defaultRoute' => '/mdpages/default/index',
+  'layout' => '@jacmoe/mdpages/views/layouts/main.php',
+  'bootstrap' => ['log'],
+  'components' => [
+    'request' => [
+      // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+      'cookieValidationKey' => 'PaldaMQ2zFEsAhv2SPRlp2it_XNnSuJz',
     ],
-    'modules' => [
-        'mdpages' => [
-            'class' => 'jacmoe\mdpages\Module',
-        ],
+    'cache' => [
+      'class' => 'yii\caching\FileCache',
     ],
-    'params' => $params,
+    'user' => [
+      'identityClass' => '\yii\web\IdentityInterface',
+    ],
+    'errorHandler' => [
+      'errorAction' => '/mdpages/default/error',
+    ],
+    'log' => [
+      'traceLevel' => YII_DEBUG ? 3 : 0,
+      'targets' => [
+        [
+          'class' => 'yii\log\FileTarget',
+          'levels' => ['error', 'warning'],
+        ],
+      ],
+    ],
+    'urlManager' => [
+      'enablePrettyUrl' => true,
+      'showScriptName' => false,
+      'rules' => [
+      ],
+    ],
+    'view' => [
+      'theme' => [
+        'basePath' => '@app/themes/default',
+        'baseUrl' => '@web/themes/default',
+        'pathMap' => [
+          '@jacmoe/mdpages/views' => '@app/themes/default/views',
+        ],
+      ],
+    ],
+  ],
+  'modules' => [
+    'mdpages' => [
+      'class' => 'jacmoe\mdpages\Module',
+    ],
+  ],
+  'params' => $params,
 ];
 
 return $config;

0789f6b1361434b25f235c7c763a2c6727c18a8e
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-20 00:26:17 +0100

    Breadcrumb work

--- themes/default/views/page/view.php
+++ themes/default/views/page/view.php
@@ -1,10 +1,21 @@
 <?php
 /* @var $this yii\web\View */
 $this->title = $metatags['title'];
+$this->params['breadcrumbs'][] = ['label' => 'Pages', 'url' => ['index']];
+if(count($parts) > 1) {
+    for($i = 0; $i < count($parts)-1; $i++) {
+        $this->params['breadcrumbs'][] = ['label' => $parts[$i], 'url' => ['index']];
+    }
+}
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <?php
 echo '<pre>';
 print_r($metatags);
+echo '</pre>';
+?>
+<?php
+echo '<pre>';
+print_r($parts);
 echo '</pre>';
 ?>

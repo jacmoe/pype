90ebd5edac4880221f87cf051f66f7a59829bf02
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-21 20:22:28 +0100

    Page list for index from Flywheel repo

--- themes/default/views/page/index.php
+++ themes/default/views/page/index.php
@@ -4,27 +4,8 @@ $this->title = 'Pages';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <?php
-$metaParser = new jacmoe\mdpages\components\Meta;
-$filter = '\jacmoe\mdpages\components\ContentFileFilterIterator';
-$files = jacmoe\mdpages\components\Utility::getFiles(\Yii::getAlias('@pages'), $filter);
-if(is_null($files)) {
-    echo 'no pages here';
-} else {
-  foreach($files as $file) {
-      $url = substr($file, strlen(\Yii::getAlias('@pages')));
-      $url = ltrim($url, '/');
-      $raw_url = pathinfo($url);
-      $url = $raw_url['filename'];
-      if($url == 'README') continue;
-      if($raw_url['dirname'] != '.') {
-          $url = $raw_url['dirname'] . '/' . $url;
-      }
-      echo yii\helpers\Html::a($url,yii\helpers\Url::to(array('page/view', 'id' => $url)));
-      echo '<br>';
-  }
-  foreach($pages as $page) {
-      echo $page->title;
-      echo '<br>';
-  }
+foreach($pages as $page) {
+  echo yii\helpers\Html::a($page->title,yii\helpers\Url::to(array('page/view', 'id' => $page->url)));
+  echo '<br>';
 }
 ?>

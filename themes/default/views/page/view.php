e0a343e1f1c4e2bf1c09f4029e12facb916e0512
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-22 00:04:07 +0100

    Sanity check in view

--- themes/default/views/page/view.php
+++ themes/default/views/page/view.php
@@ -1,6 +1,6 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = $page->title;
+$this->title = isset($page->title) ? $page->title : 'Untitled';
 $this->params['breadcrumbs'][] = ['label' => 'Pages', 'url' => ['index']];
 if(count($parts) > 1) {
     for($i = 0; $i < count($parts)-1; $i++) {

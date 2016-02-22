ec6a462d76ded06125c83fd6acb301257d1eed78
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-22 20:40:53 +0100

    Hook: now uses secret from WEBHOOK_SECRET environment variable

--- web/hook.php
+++ web/hook.php
@@ -15,7 +15,8 @@ function run($postBody, $headers) {
         echo 'No signature';
         return false;
     }
-    $secret = 'secret';
+
+    $secret = getenv('webhook_secret');
 
     $hash = 'sha1=' . hash_hmac( 'sha1', $postBody, $secret, false );
 
@@ -24,46 +25,14 @@ function run($postBody, $headers) {
         return false;
     }
 
+    // check if the push came from the right repository
+    if ($payload->repository->url == 'https://github.com/jacmoe/mdpages-pages'
+        && $payload->ref == 'refs/heads/master') {
 
-    // check if the request comes from github server
-    $github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178', '50.57.231.61');
-    //if (in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
+        // execute update script
+        passthru('/usr/local/php56/bin//php /home/jacmoe1/pype.jacmoe.dk/current/yii mdpages/pages/update');
 
-    if (1 === 1) {
-        // check if the push came from the right repository
-        if ($payload->repository->url == 'https://github.com/jacmoe/mdpages-pages') {
-
-            // execute update script, and record its output
-            passthru('/usr/local/php56/bin//php /home/jacmoe1/pype.jacmoe.dk/current/yii mdpages/pages/update');
-
-            // // prepare and send the notification email
-            // if (isset($config['email'])) {
-            //     // send mail to someone, and the github user who pushed the commit
-            //     $body = '<p>The Github user <a href="https://github.com/'
-            //     . $payload->pusher->name .'">@' . $payload->pusher->name . '</a>'
-            //     . ' has pushed to ' . $payload->repository->url
-            //     . ' and consequently, ' . $endpoint['action']
-            //     . '.</p>';
-            //
-            //     $body .= '<p>Here\'s a brief list of what has been changed:</p>';
-            //     $body .= '<ul>';
-            //     foreach ($payload->commits as $commit) {
-            //         $body .= '<li>'.$commit->message.'<br />';
-            //         $body .= '<small style="color:#999">added: <b>'.count($commit->added)
-            //             .'</b> &nbsp; modified: <b>'.count($commit->modified)
-            //             .'</b> &nbsp; removed: <b>'.count($commit->removed)
-            //             .'</b> &nbsp; <a href="' . $commit->url
-            //             . '">read more</a></small></li>';
-            //     }
-            //     $body .= '</ul>';
-            //     $body .= '<p>What follows is the output of the script:</p><pre>';
-            //     $body .= $output. '</pre>';
-            //     $body .= '<p>Cheers, <br/>Github Webhook Endpoint</p>';
-            //
-            //     mail($config['email']['to'], $endpoint['action'], $body, $headers);
-            // }
-            return true;
-        }
+        return true;
     } else {
         throw new Exception("This does not appear to be a valid requests from Github.\n");
     }

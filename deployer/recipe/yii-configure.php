c9f5985b2d0bc5696ad3186d8ba13ef1417d4da5
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-22 15:03:04 +0100

    sadf

--- deployer/recipe/yii-configure.php
+++ deployer/recipe/yii-configure.php
@@ -73,8 +73,8 @@ task('deploy:configure', function () {
                         upload($tmpFile, "$releaseDir/" . $target);
                         run('chmod +x ' . "$releaseDir/" . $target);
                     } else {
-                        run("mkdir -p $deployDir/shared/" . dirname($target));
-                        upload($tmpFile, "$deployDir/shared/" . $target);
+                        run("mkdir -p $deployDir/" . dirname($target));
+                        upload($tmpFile, "$deployDir/" . $target);
                     }
                     $success = true;
                 }

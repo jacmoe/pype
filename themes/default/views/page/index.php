760c6d8e5df7ca71c16757cbdd1bfc1a10340e92
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-22 22:39:19 +0100

    Index renders README.md if it exists

--- themes/default/views/page/index.php
+++ themes/default/views/page/index.php
@@ -3,6 +3,10 @@
 $this->title = 'Pype';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="content">
+    <?= $content; ?>
+</div>
+<hr/>
 <?php
 foreach($pages as $page) {
   echo yii\helpers\Html::a($page->title,yii\helpers\Url::to(array('page/view', 'id' => $page->url)));

3b31415cb849676267531f90a7f9df472b6f80ba
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-25 22:19:48 +0100

    Removed ASC from page sort

--- themes/default/views/layouts/main.php
+++ themes/default/views/layouts/main.php
@@ -56,7 +56,7 @@ $theme = $this->theme;
         </div>
         <div class="top-bar-right">
             <?php
-                foreach(Page::pages('updated ASC') as $page) {
+                foreach(Page::pages('updated') as $page) {
                     echo Html::a($page->title, Page::url($page->url)) . ' | ';
                 }
                 echo Html::a('RSS', Url::to('/rss'));

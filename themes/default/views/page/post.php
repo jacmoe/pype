e0254f70ce237601f5ac1cb15eb9f7323a3b224a
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-28 00:25:28 +0100

    Test alternative view

---
+++ themes/default/views/page/post.php
@@ -0,0 +1,28 @@
+<?php
+use yii\helpers\Html;
+use yii\helpers\Url;
+use jacmoe\mdpages\helpers\Page;
+/* @var $this yii\web\View */
+$this->title = isset($page->title) ? $page->title : 'Untitled';
+
+foreach($this->params['breadcrumbs'] as $breadcrumb) {
+    if(isset($breadcrumb['url'])) {
+        echo Html::a($breadcrumb['label'], $breadcrumb['url']) . '  ';
+    } else {
+        echo $breadcrumb['label'] . '  ';
+    }
+}
+?>
+<hr>
+<div class="content">
+    <?= $content; ?>
+</div>
+<div class="content">
+    <hr>
+    <h3>Contributors to this page</h3>
+    <?php
+        foreach($page->contributors as $contributor) {
+            echo Html::a(Html::img($contributor->avatar_url, array('width' => '32px', 'title' => $contributor->name)), $contributor->html_url);
+        }
+    ?>
+</div>

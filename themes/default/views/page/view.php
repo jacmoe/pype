0938cafb721a882f3107588c08528992d3141d0f
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-27 03:57:34 +0100

    Manual breadcrumbs..

--- themes/default/views/page/view.php
+++ themes/default/views/page/view.php
@@ -1,19 +1,17 @@
 <?php
 use yii\helpers\Html;
+use yii\helpers\Url;
 use jacmoe\mdpages\helpers\Page;
 /* @var $this yii\web\View */
 $this->title = isset($page->title) ? $page->title : 'Untitled';
 
-// $this->params['breadcrumbs'][] = ['label' => 'Pages', 'url' => ['index']];
-// if(count($parts) > 1) {
-//     for($i = 0; $i < count($parts)-1; $i++) {
-//         $this->params['breadcrumbs'][] = ['label' => $parts[$i], 'url' => ['index']];
-//     }
-// }
-// $this->params['breadcrumbs'][] = $this->title;
-echo '<pre>';
-print_r($this->params['breadcrumbs']);
-echo '</pre>';
+foreach($this->params['breadcrumbs'] as $breadcrumb) {
+    if(isset($breadcrumb['url'])) {
+        echo Html::a($breadcrumb['label'], $breadcrumb['url']) . ' | ';
+    } else {
+        echo $breadcrumb['label'] . ' | ';
+    }
+}
 ?>
 <div class="content">
     <?= $content; ?>

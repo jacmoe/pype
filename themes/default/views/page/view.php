a7d9197b82d373d17aff44b919954228f8876962
Jacob Moen <jacmoe.dk@gmail.com>
2016-02-28 23:14:03 +0100

    Debug code off

--- themes/default/views/page/view.php
+++ themes/default/views/page/view.php
@@ -27,7 +27,7 @@ foreach($this->params['breadcrumbs'] as $breadcrumb) {
 <div class="content">
     <pre>
     <?php
-        print_r($headings);
+        //print_r($headings);
     ?>
     </pre>
 </div>

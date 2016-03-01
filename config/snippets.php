62d4366788fc20fb17a12a0e984e93193ab8142f
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-01 03:05:18 +0100

    Snippets moved to snippet class

--- config/snippets.php
+++ config/snippets.php
@@ -1,14 +1,20 @@
 <?php
 
+class Snippets extends \jacmoe\mdpages\components\snippets\Snippets
+{
+    public function clearfix($text=null) {
+        return "<div class=\"clearfix\"></div>";
+    }
+
+    public function icondiv($icon, $clear=false) {
+        if($clear)
+        {
+          return "<div class=\"ui horizontal clearing divider\"><i class=\"fi-".$icon."\"></i></div>";
+        }
+        return "<div class=\"ui horizontal divider\"><i class=\"fi-".$icon."\"></i></div>";
+    }
+}
+
 return [
-    'clearfix' => function($text=null) {
-      return "<div class=\"clearfix\"></div>";
-    },
-    'icondiv' => function($icon, $clear=false) {
-      if($clear)
-      {
-        return "<div class=\"ui horizontal clearing divider\"><i class=\"fi-".$icon."\"></i></div>";
-      }
-      return "<div class=\"ui horizontal divider\"><i class=\"fi-".$icon."\"></i></div>";
-    },
+    new Snippets(),
 ];

5b98d083cc1d08fdb14f01be3959169f8b4b8c8a
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-01 03:16:16 +0100

    Two more snippet functions to snippet class

--- config/snippets.php
+++ config/snippets.php
@@ -1,18 +1,33 @@
 <?php
-
+//TODO: figure out how to move this snippet class to components..
+/**
+ * Custom snippets to extend the Markdown markup
+ */
 class Snippets extends \jacmoe\mdpages\components\snippets\Snippets
 {
-    public function clearfix($text=null) {
+    public function clearfix($text=null)
+    {
         return "<div class=\"clearfix\"></div>";
     }
 
-    public function icondiv($icon, $clear=false) {
-        if($clear)
-        {
+    public function icondiv($icon, $clear=false)
+    {
+        if($clear) {
           return "<div class=\"ui horizontal clearing divider\"><i class=\"fi-".$icon."\"></i></div>";
         }
         return "<div class=\"ui horizontal divider\"><i class=\"fi-".$icon."\"></i></div>";
     }
+
+    public function inimage($title, $source, $align ="left", $width=320, $height=240, $size="large")
+    {
+        return "<img src=\"".\Yii::getAlias('@app/web')."/images/".$source."\" alt=\"".$title."\" title=\"".$title."\" class=\"th gallery ".$size." ".$align."\">";
+    }
+
+    public function lightbox($title, $source, $target, $align ="left", $width=320, $height=240, $size="large")
+    {
+        return "<a href=\"".\Yii::getAlias('@app/web')."/images/".$target."\" data-lightbox=\"".$target."\" title=\"".$title."\" class=\"sb\"><img src=\"".\Yii::getAlias('@app/web')."/images/".$source."\" alt=\"".$title."\" title=\"".$title."\" class=\"gallery th ".$size." ".$align." floated image\"></a>";
+    }
+
 }
 
 return [

3ad500c97d34f6dea3deca8e6f74f0620b008a6e
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-02 22:47:35 +0100

    Avatars now use the avatar style

--- themes/primer/views/page/partials/_contributors.php
+++ themes/primer/views/page/partials/_contributors.php
@@ -1,4 +1,4 @@
 <?php
 foreach($page->contributors as $contributor) {
-    echo Html::a(Html::img(Yii::getAlias('@web/avatars/') . $contributor->login . '.png', array('width' => '24', 'height' => '24', 'title' => $contributor->name)), $contributor->html_url);
+    echo Html::a(Html::img(Yii::getAlias('@web/avatars/') . $contributor->login . '.png', array('width' => '24', 'height' => '24', 'title' => $contributor->name, 'class' => 'avatar avatar-small')), $contributor->html_url, array('target' => '_blank'));
 }

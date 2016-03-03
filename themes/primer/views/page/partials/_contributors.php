15a1d98d30b06ae83f68257ec2353ce3974bf57d
Jacob Moen <jacmoe.dk@gmail.com>
2016-03-03 14:43:47 +0100

    Contributor avatars now featuring tooltip

--- themes/primer/views/page/partials/_contributors.php
+++ themes/primer/views/page/partials/_contributors.php
@@ -1,4 +1,4 @@
 <?php
 foreach($page->contributors as $contributor) {
-    echo Html::a(Html::img(Yii::getAlias('@web/avatars/') . $contributor->login . '.png', array('width' => '24', 'height' => '24', 'title' => $contributor->name, 'class' => 'avatar avatar-small')), $contributor->html_url, array('target' => '_blank'));
+    echo Html::a(Html::img(Yii::getAlias('@web/avatars/') . $contributor->login . '.png', array('width' => '24', 'height' => '24', 'class' => 'avatar avatar-small')), $contributor->html_url, array('target' => '_blank', 'class' => 'tooltipped tooltipped-s', 'aria-label' => $contributor->login));
 }

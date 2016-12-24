3c6f415a483d8abb545f57bc2486e37e600b95c8
Jacob Moen <jacmoe.dk@gmail.com>
2016-12-24 23:01:11 +0100

    Browser-sync fixed - apparently, it has to be local too

--- themes/primer/build/gulpfile.js
+++ themes/primer/build/gulpfile.js
@@ -1,20 +1,20 @@
 /*
-* This file is part of
-*  _ __  _   _ _ __   ___
-* | '_ \| | | | '_ \ / _ \
-* | |_) | |_| | |_) |  __/
-* | .__/ \__, | .__/ \___|
-* |_|    |___/|_|
-*                 Personal Yii Page Engine
-*
-*	Copyright (c) 2016 Jacob Moen
-*	Licensed under the MIT license
-*/
+ * This file is part of
+ *  _ __  _   _ _ __   ___
+ * | '_ \| | | | '_ \ / _ \
+ * | |_) | |_| | |_) |  __/
+ * | .__/ \__, | .__/ \___|
+ * |_|    |___/|_|
+ *                 Personal Yii Page Engine
+ *
+ *	Copyright (c) 2016 Jacob Moen
+ *	Licensed under the MIT license
+ */
 
 // Load plugins
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-//var browsersync = require('browser-sync');
+var browsersync = require('browser-sync');
 var rimraf = require('rimraf');
 var yargs = require('yargs');
 var yaml = require('js-yaml');
@@ -27,43 +27,43 @@ const PRODUCTION = !!(yargs.argv.production);
 var config = loadConfig();
 
 function loadConfig() {
-  ymlFile = fs.readFileSync('config.yml', 'utf8');
-  return yaml.load(ymlFile);
+    ymlFile = fs.readFileSync('config.yml', 'utf8');
+    return yaml.load(ymlFile);
 }
 
 var sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'expanded',
-  includePaths: config.PATHS.sass
+    errLogToConsole: true,
+    outputStyle: 'expanded',
+    includePaths: config.PATHS.sass
 };
 
 var autoprefixerOptions = {
-  browsers: config.COMPATIBILITY
+    browsers: config.COMPATIBILITY
 };
 
 // Styles
 function styles() {
-  return gulp.src(config.PATHS.src +'/scss/all.scss')
-    .pipe($.sourcemaps.init())
-    .pipe($.sass(sassOptions).on('error', $.sass.logError))
-    .pipe($.autoprefixer(autoprefixerOptions))
-    .pipe($.if(PRODUCTION, $.rename({ suffix: '.min' })))
-    .pipe($.if(PRODUCTION, $.cssnano()))
-    .pipe($.if(!PRODUCTION, $.sourcemaps.write('.', { sourceRoot: '../../assets/src/scss/' })))
-    .pipe(gulp.dest(config.PATHS.dist + '/css'))
-    .pipe($.notify({ message: 'Styles task complete' }));
+    return gulp.src(config.PATHS.src +'/scss/all.scss')
+        .pipe($.sourcemaps.init())
+        .pipe($.sass(sassOptions).on('error', $.sass.logError))
+        .pipe($.autoprefixer(autoprefixerOptions))
+        .pipe($.if(PRODUCTION, $.rename({ suffix: '.min' })))
+        .pipe($.if(PRODUCTION, $.cssnano()))
+        .pipe($.if(!PRODUCTION, $.sourcemaps.write('.', { sourceRoot: '../../assets/src/scss/' })))
+        .pipe(gulp.dest(config.PATHS.dist + '/css'))
+        .pipe($.notify({ message: 'Styles task complete' }));
 };
 
 // Scripts
 function scripts() {
-  return gulp.src(config.PATHS.javascript)
-    .pipe($.sourcemaps.init())
-    .pipe($.concat('all.js'))
-    .pipe($.if(PRODUCTION, $.rename({ suffix: '.min' })))
-    .pipe($.if(PRODUCTION, $.uglify()))
-    .pipe($.if(!PRODUCTION, $.sourcemaps.write('.', { sourceRoot: '../../assets/src/js/' })))
-    .pipe(gulp.dest(config.PATHS.dist + '/js'))
-    .pipe($.notify({ message: 'Scripts task complete' }));
+    return gulp.src(config.PATHS.javascript)
+        .pipe($.sourcemaps.init())
+        .pipe($.concat('all.js'))
+        .pipe($.if(PRODUCTION, $.rename({ suffix: '.min' })))
+        .pipe($.if(PRODUCTION, $.uglify()))
+        .pipe($.if(!PRODUCTION, $.sourcemaps.write('.', { sourceRoot: '../../assets/src/js/' })))
+        .pipe(gulp.dest(config.PATHS.dist + '/js'))
+        .pipe($.notify({ message: 'Scripts task complete' }));
 };
 
 // sprites
@@ -81,14 +81,14 @@ function sprites() {
 
 // Copy fonts
 function fonts() {
-  return gulp.src(config.PATHS.fonts)
-    .pipe(gulp.dest(config.PATHS.dist + '/fonts'));
+    return gulp.src(config.PATHS.fonts)
+        .pipe(gulp.dest(config.PATHS.dist + '/fonts'));
 };
 
 // Copy images
 function images() {
-  return gulp.src(config.PATHS.images)
-    .pipe(gulp.dest(config.PATHS.dist + '/img'));
+    return gulp.src(config.PATHS.images)
+        .pipe(gulp.dest(config.PATHS.dist + '/img'));
 };
 
 // Clean
@@ -98,28 +98,28 @@ function clean(done) {
 
 // The main build task
 gulp.task('build', gulp.series(
-  clean,
-  sprites,
-  gulp.parallel(styles, scripts, fonts, images)
+    clean,
+    sprites,
+    gulp.parallel(styles, scripts, fonts, images)
 ));
 
 // Watch
 function watch() {
 
-  // Initialize Browsersync
-  browsersync.init({
-    proxy: config.PROXY
-  });
+    // Initialize Browsersync
+    browsersync.init({
+        proxy: config.PROXY
+    });
 
-  // Watch .scss files
-  gulp.watch(config.PATHS.src + '/scss/**/*.scss', styles);
-  // Watch .js files
-  gulp.watch(config.PATHS.src + '/js/**/*.js', scripts);
-  // Watch any view files in 'views', reload on change
-  gulp.watch(['views/**/*.php']).on('change', browsersync.reload);
-  // Watch any files in 'assets/dist', reload on change
-  gulp.watch([config.PATHS.dist + '/js/*']).on('change', browsersync.reload);
-  gulp.watch([config.PATHS.dist + '/css/*']).on('change', browsersync.reload);
+    // Watch .scss files
+    gulp.watch(config.PATHS.src + '/scss/**/*.scss', styles);
+    // Watch .js files
+    gulp.watch(config.PATHS.src + '/js/**/*.js', scripts);
+    // Watch any view files in 'views', reload on change
+    gulp.watch(['views/**/*.php']).on('change', browsersync.reload);
+    // Watch any files in 'assets/dist', reload on change
+    gulp.watch([config.PATHS.dist + '/js/*']).on('change', browsersync.reload);
+    gulp.watch([config.PATHS.dist + '/css/*']).on('change', browsersync.reload);
 };
 
 // Default task runs build and then watch
